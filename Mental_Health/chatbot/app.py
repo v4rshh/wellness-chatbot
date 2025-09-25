c9f18e0cab947dd66db607b8177fbf1da77b8068
varshith Nagubandi <nagubandivarshith24bcs63@iiitkottayam.ac.in>
2025-09-25 15:14:06 +0530

    updated

--- Mental_Health/chatbot/app.py
+++ Mental_Health/chatbot/app.py
@@ -23,38 +23,37 @@ except FileNotFoundError:
     print("Error: Model or data files not found. Please run training.py first.")
     exit()
 
-#Helper Functions
 def clean_up_sentence(sentence):
-    sentence_words = nltk.word_tokenize(sentence)
-    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    sentence_words=nltk.word_tokenize(sentence)
+    sentence_words=[lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     return sentence_words
 
 def bag_of_words(sentence):
-    sentence_words = clean_up_sentence(sentence)
-    bag = [0] * len(words)
+    sentence_words=clean_up_sentence(sentence)
+    bag=[0]*len(words)
     for w in sentence_words:
-        for i, word in enumerate(words):
-            if word == w:
-                bag[i] = 1
+        for i,word in enumerate(words):
+            if word==w:
+                bag[i]=1
     return np.array(bag)
 
 def predict_class(sentence):
-    bow = bag_of_words(sentence)
-    res = model.predict(np.array([bow]))[0]
-    ERROR_THRESHOLD = 0.25
-    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
-    results.sort(key=lambda x: x[1], reverse=True)
+    bow=bag_of_words(sentence)
+    res=model.predict(np.array([bow]))[0]
+    ERROR_THRESHOLD=0.25
+    results=[[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    results.sort(key=lambda x: x[1],reverse=True)
     
-    return_list = []
+    return_list=[]
     for r in results:
-        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
+        return_list.append({'intent':classes[r[0]], 'probability':str(r[1])})
     
     if not return_list:
-        return_list.append({'intent': 'default', 'probability': '1.0'})
+        return_list.append({'intent':'default','probability': '1.0'})
         
     return return_list
 
-def get_response(intents_list, intents_json):
+def get_response(intents_list,intents_json):
     tag=intents_list[0]['intent']
     list_of_intents=intents_json['intents']
     result="I'm sorry,I don't understand,Ask me anything related to depression,stress management,motivation,meditation."#Default fallback response
@@ -75,7 +74,7 @@ def chat():
     response_text=get_response(ints, intents)
     return jsonify({"response": response_text})
 
-#Main Execution
+
 if __name__=="__main__":
     print("Chatbot API is running.")
     app.run(host='0.0.0.0', port=5000)
--- Mental_Health/chatbot/training.py
+++ Mental_Health/chatbot/training.py
@@ -22,7 +22,7 @@ intents=json.loads(open('intents.json').read())
 words=[]
 classes=[]
 documents=[]
-ignore_letters=['?', '!', '.', ',']
+ignore_letters=['?','!','.',',']
 
 #Processing each intent and its patterns
 for intent in intents['intents']:
@@ -42,47 +42,46 @@ words=sorted(list(set(words)))
 
 
 classes=sorted(list(set(classes)))
-pickle.dump(words, open('words.pkl', 'wb'))
-pickle.dump(classes, open('classes.pkl', 'wb'))
-
-#Create training data
+pickle.dump(words,open('words.pkl', 'wb'))
+pickle.dump(classes,open('classes.pkl', 'wb'))
+ 
 training=[]
 output_empty=[0]*len(classes)
 
 for document in documents:
-    bag = []
-    word_patterns = document[0]
-    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+    bag=[]
+    word_patterns=document[0]
+    word_patterns=[lemmatizer.lemmatize(word.lower()) for word in word_patterns]
     for word in words:
         bag.append(1) if word in word_patterns else bag.append(0)
 
-    output_row = list(output_empty)
-    output_row[classes.index(document[1])] = 1
+    output_row=list(output_empty)
+    output_row[classes.index(document[1])]=1
     training.append([bag, output_row])
 
 # Shuffle training data
 random.shuffle(training)
 training=np.array(training, dtype=object)
 
-train_x = list(training[:, 0])
-train_y = list(training[:, 1])
+train_x=list(training[:,0])
+train_y=list(training[:,1])
 
 # Build nn model
-model = Sequential()
+model=Sequential()
 model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
 
-# Compiling model
-sgd = SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
-model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+#Compiling model
+sgd=SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy',optimizer=sgd, metrics=['accuracy'])
 
-# Train model
-hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+#Training model
+hist=model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
 
-# Save the trained model
-model.save('chatbot_model.h5', hist)
+# Save trained model
+model.save('chatbot_model.h5',hist)
 
 print("Training complete. Model saved to chatbot_model.h5")
